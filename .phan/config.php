6b3ef1d2f973de4ff06004a4b5b8d628e051de78
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-08-11 17:47:29 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 44.0.0
    The following sniffs are failing and were disabled:
    * Generic.CodeAnalysis.AssignmentInCondition.Found
    * Generic.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
    
    The following sniffs now pass and were enabled:
    * Generic.CodeAnalysis.AssignmentInCondition
    
    Temporarily disable phan's UnusedPluginSuppression rule so we can test
    in PHP 8.3.
    
    Change-Id: I75b8348a01997b61bf8fe7a2bf0022d67ee83807

--- .phan/config.php
+++ .phan/config.php
@@ -2,6 +2,6 @@
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 $cfg['directory_list'] = [ 'src' /*,'tests'*/ ];
-$cfg['suppress_issue_types'] = [];
+$cfg['suppress_issue_types'] = [ 'UnusedPluginSuppression' ];
 
 return $cfg;
