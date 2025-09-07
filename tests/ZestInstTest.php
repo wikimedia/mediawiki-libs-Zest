f37384b222a361b053c4879afa8fb2ca0880646b
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2025-09-07 19:37:12 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 48.0.0
    Change-Id: I75c6df5c75a8ce638fe0bdb76f601cfd92efc3ef

--- tests/ZestInstTest.php
+++ tests/ZestInstTest.php
@@ -15,7 +15,6 @@ class ZestInstTest extends \PHPUnit\Framework\TestCase {
 	 * This is just a simple check to make sure your library has no
 	 * syntax error. This helps you troubleshoot any typo before you
 	 * even use this library in a real project.
-	 *
 	 */
 	public function testIsThereAnySyntaxError() {
 		$var = new ZestInst;
